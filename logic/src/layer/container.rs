86bccaae4c99de7d0c201929f1833a03487b8b5c
seungha.me@gmail.com <Seungha Kim>
2021-02-24 10:50:53 +0900

    Apply traits to container

--- logic/src/layer/container.rs
+++ logic/src/layer/container.rs
@@ -1,3 +1,4 @@
+use super::traits::*;
 use crate::layer::common::*;
 
 pub struct ContainerProps {
@@ -6,6 +7,34 @@ pub struct ContainerProps {
     pub fill: Option<Fill>,
     pub opacity: f32,
     pub children: Vec<LayerId>,
+}
+
+impl DimensionLayer for ContainerProps {
+    fn content_rect(&self) -> Rect {
+        self.content_rect
+    }
+
+    fn effective_rect(&self) -> Rect {
+        self.content_rect
+    }
+}
+
+impl BorderLayer for ContainerProps {
+    fn border(&self) -> Option<Border> {
+        self.border
+    }
+}
+
+impl TransparentLayer for ContainerProps {
+    fn opacity(&self) -> f32 {
+        self.opacity
+    }
+}
+
+impl FillableLayer for ContainerProps {
+    fn fill(&self) -> Option<Fill> {
+        self.fill
+    }
 }
 
 pub enum ContainerUpdateCommand {}
