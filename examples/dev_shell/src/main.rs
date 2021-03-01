7fa5a038953f966488bcb69112c6cd52c8970767
seungha.me@gmail.com <Seungha Kim>
2021-03-01 21:44:45 +0900

    여러 어플리케이션을 만들 수 있는 구조로 변경

--- examples/dev_shell/src/main.rs
+++ examples/dev_shell/src/main.rs
@@ -1,3 +1,181 @@
-fn main() {
-    scene_controller::main();
+use layer_model::rect::RectProps;
+use layer_model::*;
+use scene_controller::*;
+use winit::event::{ElementState, VirtualKeyCode, WindowEvent};
+use winit::event::{Event, KeyboardInput};
+use winit::event_loop::{ControlFlow, EventLoop};
+
+struct SampleLogic {}
+
+impl SceneLogic for SampleLogic {
+    fn initialize_scene(&mut self, layer_repo: &mut LayerRepository) {
+        let root_layer_id = *layer_repo.root_layer_id();
+        layer_repo.create_sample_layer(
+            &root_layer_id,
+            &Rect::new(Point::new(0.0, 0.0), layer_model::Size::new(100.0, 100.0)),
+        );
+        layer_repo.create_sample_layer(
+            &root_layer_id,
+            &Rect::new(Point::new(50.0, 50.0), layer_model::Size::new(200.0, 200.0)),
+        );
+        layer_repo.create_sample_layer(
+            &root_layer_id,
+            &Rect::new(
+                Point::new(100.0, 100.0),
+                layer_model::Size::new(300.0, 300.0),
+            ),
+        );
+        layer_repo.create_rect_layer(
+            &root_layer_id,
+            RectProps {
+                content_rect: Rect::new(
+                    Point::new(0.0, 200.0),
+                    layer_model::Size::new(100.0, 100.0),
+                ),
+                opacity: 1.0,
+                fill: Some(Fill::Color {
+                    r: 0,
+                    g: 255,
+                    b: 0,
+                    a: 255,
+                }),
+                border: Some(Border {
+                    fill: Fill::Color {
+                        r: 0,
+                        g: 0,
+                        b: 255,
+                        a: 255,
+                    },
+                    width: 10.0,
+                    position: BorderPosition::Inner,
+                }),
+            },
+        );
+
+        let container_id = layer_repo.create_layer(
+            &root_layer_id,
+            Layer::Container(ContainerProps {
+                content_rect: Rect::new(
+                    Point::new(200.0, 200.0),
+                    layer_model::Size::new(100.0, 100.0),
+                ),
+                opacity: 0.5,
+                fill: Some(Fill::Color {
+                    r: 0,
+                    g: 255,
+                    b: 0,
+                    a: 255,
+                }),
+                border: Some(Border {
+                    fill: Fill::Color {
+                        r: 0,
+                        g: 0,
+                        b: 255,
+                        a: 255,
+                    },
+                    width: 10.0,
+                    position: BorderPosition::Inner,
+                }),
+                children: vec![],
+            }),
+        );
+
+        layer_repo.create_rect_layer(
+            &container_id,
+            RectProps {
+                content_rect: Rect::new(
+                    Point::new(100.0, 100.0),
+                    layer_model::Size::new(100.0, 100.0),
+                ),
+                opacity: 1.0,
+                fill: Some(Fill::Color {
+                    r: 0,
+                    g: 255,
+                    b: 0,
+                    a: 255,
+                }),
+                border: Some(Border {
+                    fill: Fill::Color {
+                        r: 0,
+                        g: 0,
+                        b: 255,
+                        a: 255,
+                    },
+                    width: 10.0,
+                    position: BorderPosition::Inner,
+                }),
+            },
+        );
+    }
+
+    fn handle_window_event(
+        &mut self,
+        window_event: &WindowEvent,
+        layer_repo: &mut LayerRepository,
+    ) -> ControlFlow {
+        match window_event {
+            WindowEvent::CloseRequested => ControlFlow::Exit,
+            WindowEvent::KeyboardInput { input, .. } => match input {
+                KeyboardInput {
+                    state: ElementState::Pressed,
+                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                    ..
+                } => ControlFlow::Exit,
+                _ => ControlFlow::Poll,
+            },
+            WindowEvent::CursorMoved { position, .. } => {
+                log::debug!("cursor moved: {}, {}", position.x, position.y);
+                ControlFlow::Poll
+            }
+            WindowEvent::Resized(physical_size) => {
+                // TODO: resize
+                // scene_controller.handle_resize(*physical_size);
+                ControlFlow::Poll
+            }
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                // TODO: resize
+                // scene_controller.handle_resize(**new_inner_size);
+                ControlFlow::Poll
+            }
+            _ => ControlFlow::Poll,
+        }
+    }
+
+    fn update(&mut self, layer_repo: &mut LayerRepository) {}
+}
+
+pub fn main() {
+    let initial_width = 400.;
+    let initial_height = 400.;
+    // TODO: 400 에서 줄이면 에러:
+    // copy would end up overruning the bounds of one of the buffers or textures
+    let event_loop = EventLoop::new();
+    env_logger::init();
+
+    let mut logic = SampleLogic {};
+
+    let mut scene_controller = SceneController::new(&event_loop, 400.0, 400.0, logic);
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == scene_controller.window.id() => {
+            // TODO: 원래는 이런 코드가 있었음. 카메라 컨트롤 관련인데 아마도 learn wgpu 문서에 나와있을듯
+            // if !quad_renderer.input(event) {
+            *control_flow = scene_controller
+                .logic
+                .handle_window_event(event, &mut scene_controller.layer_repository);
+        }
+        Event::RedrawRequested(_) => {
+            scene_controller.update();
+            scene_controller.render();
+        }
+        Event::MainEventsCleared => {
+            // RedrawRequested will only trigger once, unless we manually
+            // request it.
+            scene_controller.window.request_redraw();
+        }
+        _ => {}
+    });
 }
--- scene_controller/src/lib.rs
+++ scene_controller/src/lib.rs
@@ -1,6 +1,7 @@
 use futures::executor::block_on;
 use layer_model::commands::LayerCreationCommand;
 use layer_model::rect::RectProps;
+use layer_model::Layer::Container;
 use layer_model::*;
 use raqote::SolidSource;
 use wgpu_renderer::wgpu_layer::*;
@@ -12,192 +13,57 @@ use winit::{
     window::WindowBuilder,
 };
 
-pub fn main() {
-    let initial_width = 400.;
-    let initial_height = 400.;
-    // TODO: 400 에서 줄이면 에러:
-    // copy would end up overruning the bounds of one of the buffers or textures
-
-    env_logger::init();
-    let event_loop = EventLoop::new();
-
-    let mut scene_controller = SceneController::new(
-        WindowBuilder::new()
-            .with_inner_size(Size::Logical(LogicalSize::new(
-                initial_width,
-                initial_height,
-            )))
-            .build(&event_loop)
-            .unwrap(),
-    );
-    let root_layer_id = *scene_controller.layer_repository.root_layer_id();
-    scene_controller.layer_repository.create_sample_layer(
-        &root_layer_id,
-        &Rect::new(Point::new(0.0, 0.0), layer_model::Size::new(100.0, 100.0)),
-    );
-    scene_controller.layer_repository.create_sample_layer(
-        &root_layer_id,
-        &Rect::new(Point::new(50.0, 50.0), layer_model::Size::new(200.0, 200.0)),
-    );
-    scene_controller.layer_repository.create_sample_layer(
-        &root_layer_id,
-        &Rect::new(
-            Point::new(100.0, 100.0),
-            layer_model::Size::new(300.0, 300.0),
-        ),
-    );
-    scene_controller.layer_repository.create_rect_layer(
-        &root_layer_id,
-        RectProps {
-            content_rect: Rect::new(Point::new(0.0, 200.0), layer_model::Size::new(100.0, 100.0)),
-            opacity: 1.0,
-            fill: Some(Fill::Color {
-                r: 0,
-                g: 255,
-                b: 0,
-                a: 255,
-            }),
-            border: Some(Border {
-                fill: Fill::Color {
-                    r: 0,
-                    g: 0,
-                    b: 255,
-                    a: 255,
-                },
-                width: 10.0,
-                position: BorderPosition::Inner,
-            }),
-        },
-    );
-
-    let container_id = scene_controller.layer_repository.create_layer(
-        &root_layer_id,
-        Layer::Container(ContainerProps {
-            content_rect: Rect::new(
-                Point::new(200.0, 200.0),
-                layer_model::Size::new(100.0, 100.0),
-            ),
-            opacity: 0.5,
-            fill: Some(Fill::Color {
-                r: 0,
-                g: 255,
-                b: 0,
-                a: 255,
-            }),
-            border: Some(Border {
-                fill: Fill::Color {
-                    r: 0,
-                    g: 0,
-                    b: 255,
-                    a: 255,
-                },
-                width: 10.0,
-                position: BorderPosition::Inner,
-            }),
-            children: vec![],
-        }),
-    );
-
-    scene_controller.layer_repository.create_rect_layer(
-        &container_id,
-        RectProps {
-            content_rect: Rect::new(
-                Point::new(100.0, 100.0),
-                layer_model::Size::new(100.0, 100.0),
-            ),
-            opacity: 1.0,
-            fill: Some(Fill::Color {
-                r: 0,
-                g: 255,
-                b: 0,
-                a: 255,
-            }),
-            border: Some(Border {
-                fill: Fill::Color {
-                    r: 0,
-                    g: 0,
-                    b: 255,
-                    a: 255,
-                },
-                width: 10.0,
-                position: BorderPosition::Inner,
-            }),
-        },
-    );
-
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == scene_controller.window.id() => {
-            // TODO: 원래는 이런 코드가 있었음. 카메라 컨트롤 관련인데 아마도 learn wgpu 문서에 나와있을듯
-            // if !quad_renderer.input(event) {
-            match event {
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                WindowEvent::KeyboardInput { input, .. } => match input {
-                    KeyboardInput {
-                        state: ElementState::Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Escape),
-                        ..
-                    } => *control_flow = ControlFlow::Exit,
-                    _ => {}
-                },
-                WindowEvent::CursorMoved { position, .. } => {
-                    log::debug!("cursor moved: {}, {}", position.x, position.y);
-                }
-                WindowEvent::Resized(physical_size) => {
-                    scene_controller.handle_resize(*physical_size);
-                }
-                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                    scene_controller.handle_resize(**new_inner_size);
-                }
-                _ => {}
-            }
-        }
-        Event::RedrawRequested(_) => {
-            scene_controller.update();
-            scene_controller.render();
-        }
-        Event::MainEventsCleared => {
-            // RedrawRequested will only trigger once, unless we manually
-            // request it.
-            scene_controller.window.request_redraw();
-        }
-        _ => {}
-    });
+pub trait SceneLogic {
+    fn initialize_scene(&mut self, layer_repo: &mut LayerRepository);
+    fn handle_window_event(
+        &mut self,
+        window_event: &WindowEvent,
+        layer_repo: &mut LayerRepository,
+    ) -> ControlFlow;
+    fn update(&mut self, layer_repo: &mut LayerRepository);
 }
 
-pub struct SceneController {
+pub struct SceneController<L: SceneLogic> {
     pub window: Window,
     quad_renderer: QuadRenderer,
-    layer_repository: LayerRepository,
+    pub layer_repository: LayerRepository,
     // TODO: 지금은 텍스처 하나에 전부 다 그리지만, 개선되어야 함
     root_quad_id: QuadId,
     root_draw_target: raqote::DrawTarget,
+    pub logic: L,
 }
 
-impl SceneController {
-    fn new(window: Window) -> Self {
+impl<L: SceneLogic> SceneController<L> {
+    pub fn new(event_loop: &EventLoop<()>, width: f64, height: f64, logic: L) -> Self {
+        let window = WindowBuilder::new()
+            .with_inner_size(Size::Logical(LogicalSize::new(width, height)))
+            .build(&event_loop)
+            .unwrap();
+        let mut logic = logic;
         let width = window.inner_size().width as f32;
         let height = window.inner_size().height as f32;
         let mut quad_renderer = block_on(QuadRenderer::new(&window));
         let root_quad_id = quad_renderer.new_quad(-200.0, -200.0, width, height);
-        let layer_repository = LayerRepository::new(layer_model::common::Size::new(width, height));
+        let mut layer_repository =
+            LayerRepository::new(layer_model::common::Size::new(width, height));
         let root_draw_target = raqote::DrawTarget::new(width as i32, height as i32);
+        logic.initialize_scene(&mut layer_repository);
         SceneController {
             window,
             quad_renderer,
             root_quad_id,
             root_draw_target,
             layer_repository,
+            logic,
         }
     }
 
-    fn update(&mut self) {
+    pub fn update(&mut self) {
+        self.logic.update(&mut self.layer_repository);
         self.quad_renderer.update();
     }
 
-    fn render(&mut self) {
+    pub fn render(&mut self) {
         // TODO: 매번 clear 하지 말아야 함
         self.root_draw_target.clear(SolidSource {
             r: 0,
